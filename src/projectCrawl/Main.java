770f7451e91056fda2555382d3974e56a1f6722c
Tawil, Hani (k1502739) <hani.tawil@kcl.ac.uk>
2016-11-10 16:39:43 +0000

    Update Main.java

--- src/projectCrawl/Main.java
+++ src/projectCrawl/Main.java
@@ -3,13 +3,8 @@ package projectCrawl;
 public class Main {
 
 	public static void main(String[] args) {
-
-<<<<<<< HEAD
 		System.out.println("Hani");
-=======
-		System.out.println("This is Noah");
->>>>>>> branch 'master' of https://github.kcl.ac.uk/k1502739/projectCrawl.git
-		
+
 	}
 
 }
